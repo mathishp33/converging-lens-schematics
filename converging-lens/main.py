f05e636d68212241e031be5f2f59ce451168c189
mathishp33 <69140979+mathishp33@users.noreply.github.com>
2024-03-25 20:45:46 +0100

    Update main.py

--- converging-lens/main.py
+++ converging-lens/main.py
@@ -1,6 +1,5 @@
 import pygame
 import pickle
-from math import *
 import math
 # Define some colors
 BLACK = (0, 0, 0)
@@ -24,10 +23,10 @@ clock = pygame.time.Clock()
 file = open('data', 'rb')
 data = pickle.load(file)
 
-f = int(data[0])
-AB = int(data[1])
-OA = int(data[2])
-scale = int(data[3])
+f = float(data[0])
+AB = float(data[1])
+OA = float(data[2])
+scale = float(data[3])
 OA2 = (f*OA)/(f+OA)
 gamma = OA2 / OA
 AB2 = AB * gamma
@@ -49,13 +48,13 @@ print("vergence : ", (1/f*scale))
 
 def regle2et3():
     
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 - OA,HEIGHT//2 - AB), (WIDTH//2 - f ,HEIGHT//2), 4) 
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 - f ,HEIGHT//2), (WIDTH//2 - f + x,HEIGHT//2 + AB2), 4) 
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 - f + x,HEIGHT//2 + AB2), (WIDTH//2 +OA ,HEIGHT//2 + AB2), 4) 
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 - OA,HEIGHT/2 - AB), (WIDTH//2 - f ,HEIGHT/2), 4) 
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 - f ,HEIGHT/2), (WIDTH/2 - f + x,HEIGHT/2 + AB2), 4) 
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 - f + x,HEIGHT/2 + AB2), (WIDTH/2 +OA ,HEIGHT/2 + AB2), 4) 
     
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 + OA2,HEIGHT//2 + AB2), (WIDTH//2 + f ,HEIGHT//2), 4) 
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 + f ,HEIGHT//2), (WIDTH//2 + f - x2,HEIGHT//2 - AB), 4) 
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 + f - x2,HEIGHT//2 - AB), (WIDTH//2 -OA2 ,HEIGHT//2 - AB), 4) 
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 + OA2,HEIGHT/2 + AB2), (WIDTH/2 + f ,HEIGHT/2), 4) 
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 + f ,HEIGHT/2), (WIDTH/2 + f - x2,HEIGHT/2 - AB), 4) 
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 + f - x2,HEIGHT/2 - AB), (WIDTH/2 -OA2 ,HEIGHT/2 - AB), 4) 
     
 
 
@@ -71,14 +70,14 @@ while running:
     screen.fill(BLACK)
     
     
-    pygame.draw.line(screen, WHITE, (0, HEIGHT//2), (WIDTH, HEIGHT//2),3) #axe optique
-    pygame.draw.line(screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT),5)  #lentille convergente
-    pygame.draw.line(screen, GREEN, (WIDTH//2 - OA,HEIGHT//2), (WIDTH//2 - OA,HEIGHT//2 - AB), 2) #foyer objet AB
-    pygame.draw.line(screen, WHITE, (WIDTH//2 - f,HEIGHT//2 - 20), (WIDTH//2 - f,HEIGHT//2 + 20), 4) #OF = f' 
-    pygame.draw.line(screen, WHITE, (WIDTH//2 + f,HEIGHT//2 - 20), (WIDTH//2 + f,HEIGHT//2 + 20), 4) #OF' = f'
-    pygame.draw.line(screen, RED, (WIDTH//2 + OA2,HEIGHT//2), (WIDTH//2 + OA2,HEIGHT//2 + AB2), 4) #foyer image A'B'
+    pygame.draw.line(screen, WHITE, (0, HEIGHT/2), (WIDTH, HEIGHT/2),3) #axe optique
+    pygame.draw.line(screen, WHITE, (WIDTH/2, 0), (WIDTH/2, HEIGHT),5)  #lentille convergente
+    pygame.draw.line(screen, GREEN, (WIDTH/2 - OA,HEIGHT/2), (WIDTH/2 - OA,HEIGHT/2 - AB), 2) #foyer objet AB
+    pygame.draw.line(screen, WHITE, (WIDTH/2 - f,HEIGHT/2 - 20), (WIDTH/2 - f,HEIGHT/2 + 20), 4) #OF = f' 
+    pygame.draw.line(screen, WHITE, (WIDTH/2 + f,HEIGHT/2 - 20), (WIDTH/2 + f,HEIGHT/2 + 20), 4) #OF' = f'
+    pygame.draw.line(screen, RED, (WIDTH/2 + OA2,HEIGHT/2), (WIDTH/2 + OA2,HEIGHT/2 + AB2), 4) #foyer image A'B'
     
-    pygame.draw.line(screen, YELLOW, (WIDTH//2 - OA,HEIGHT//2 - AB), (WIDTH//2 + OA2,HEIGHT//2 + AB2), 4) #1ere regle
+    pygame.draw.line(screen, YELLOW, (WIDTH/2 - OA,HEIGHT//2 - AB), (WIDTH/2 + OA2,HEIGHT/2 + AB2), 4) #1ere regle
     regle2et3()
     
     
